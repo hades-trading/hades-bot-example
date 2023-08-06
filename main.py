e7d676fa62ef26718254a3cd6a2792fb375114ee
Kuo Guo <gary.guo.china@gmail.com>
2023-08-06 17:43:01 +0800

    upgrade to hades-core 0.0.3

--- main.py
+++ main.py
@@ -1,46 +1,49 @@
-import signal
 import asyncio
 import logging
 import platform
-import json
-from typing import List
-from hades.core import *
-import pandas as pd
-import json
+import signal
+from datetime import datetime
 from typing import List
 
+import pandas as pd
+from hades.core import Strategy, Messenger, Order, Position, Tick, TradeBotConf, MessageFactory, MessageEnum, \
+    TradeExecutor, ExchangeEnum
 
 
 class NotificationStrategy(Strategy):
-    def __init__(self, messager: Messager) -> None:
-        super().__init__(id='a4e16024-ec4c-42f6-a6ad-845419df0788', 
-                         # Binance is BTCUSDT
-                         # OKX is BTC-USDT-SWAP
-                         symbols=['BTCUSDT'],
-                         instrumentType='SWAP', 
+    def __init__(self, messenger: Messenger) -> None:
+        super().__init__(strategy_id='a4e16024-ec4c-42f6-a6ad-845419df0788',
+                         symbols=['BTCUSDT'],  # Binance is BTCUSDT, OKX is BTC-USDT-SWAP
+                         instrument_type='SWAP',
                          klines=['1m'])
-        self.messager = messager
+        self.messenger = messenger
 
     def on_order_status(self, orders: List[Order]):
+        now = datetime.now().strftime("%H:%M:%S")
         for order in orders:
-            self.messager.notify(f'{json.dumps(order._asdict(), default=str)}')
+            if order.status == 'TRADE':
+                self.messenger.notify(f'{now}-Order Filled')
+            elif order.status == 'NEW':
+                self.messenger.notify(f'{now}-New Order')
+            elif order.status == 'CANCELED':
+                self.messenger.notify(f'{now}-Order Cancelled')
 
     def on_position_status(self, positions: List[Position]):
         super().on_position_status(positions)
         for pos in positions:
             if pos.unrealized_profit_ratio < -20:
-                self.messager.notify_with_interval(f'{json.dumps(pos._asdict(), default=str)}')
+                self.messenger.notify_with_interval(
+                    f'{datetime.now().strftime("%H:%M:%S")}-{pos.unrealized_profit_ratio}', 60 * 3)
 
     def on_tick(self, ticks: List[Tick]):
         super().on_tick(ticks)
-        sample = [tick._asdict() for tick in self.ticks[-200:]]
-        df = pd.DataFrame(sample)
-        if df.price.std() > 15:
-            self.messager.notify_with_interval(f'price rapid change, current = {sample[-1]["price"]}')
+        df = pd.DataFrame([tick.model_dump() for tick in self.ticks[-200:]])
+        if df.price.std() > 30:
+            self.messenger.notify_with_interval(
+                f'{datetime.now().strftime("%H:%M:%S")} rapid price change, current = {self.ticks[-1].price}')
 
 
-
-async def shutdown(sig: signal.Signals) -> None:
+async def shutdown() -> None:
     tasks = []
     for task in asyncio.all_tasks(loop):
         if task is not asyncio.current_task(loop):
@@ -50,23 +53,24 @@ async def shutdown(sig: signal.Signals) -> None:
     print("Finished awaiting cancelled tasks, results: {0}".format(results))
     loop.stop()
 
+
 if __name__ == '__main__':
     logging.info('[1] init conf')
     conf = TradeBotConf.load()
 
-    logging.info('[2] init messager')
-    messager = Messager(conf)
+    logging.info('[2] init messenger')
+    _messenger = MessageFactory.build_messenger(MessageEnum.DingDing, conf)
 
-    logging.info('[3] init stragety')
-    stragety = NotificationStrategy(messager)
-  
+    logging.info('[3] init strategy')
+    strategy = NotificationStrategy(_messenger)
+
     logging.info('[4] init executor')
-    executor = TradeExecutor(stragety, backtesting=False, exchange=ExchangeEnum.Binance)
+    executor = TradeExecutor(strategy, exchange=ExchangeEnum.Binance)
 
     loop = asyncio.get_event_loop()
     if platform.platform().find('Windows') == -1:
         for sig in [signal.SIGINT, signal.SIGTERM]:
-            loop.add_signal_handler(sig, lambda: asyncio.create_task(shutdown(sig)))
+            loop.add_signal_handler(sig, lambda: asyncio.create_task(shutdown()))
     try:
         loop.create_task(executor.execute())
         loop.run_forever()
